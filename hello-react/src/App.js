94ef5d9e901e4af1baf87996badbd100f1256f7e
saimall <rameshwaramsaimallik@gmail.com>
2023-03-19 20:06:15 +0530

    Intial React setup-project

---
+++ hello-react/src/App.js
@@ -0,0 +1,66 @@
+import TaskCard from "./TaskCard";
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Smarter Tasks</h1>
+      <h2 class="font-bold mb-2">
+        Project:{" "}
+        <span class="text-gray-600">
+          Graduation Final Year Project(Revamp College Website)
+        </span>
+      </h2>
+      <div class="flex justify-between">
+        <div class="w-1/2 mr-2 rounded-lg shadow-md p-4 mb-2 status">
+          <h3 class="text-xl font-bold mb-2">Pending</h3>
+          <TaskCard
+            title="Build the Website with static content"
+            dueDate="2013-03-19"
+            completedAtDate="2023-03-20"
+            assigneeName="Rohit S"
+          />
+          <TaskCard
+            title="Add Blog"
+            dueDate="2023-03-19"
+            completedAtDate="2023-03-20"
+            assigneeName="Rohit M"
+          />
+
+          <button class="bg-green-500 hover:bg-green-800 text-white font-bold py-2 px-4 rounded">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="inline-block h-4 w-4 align-text-bottom mr-2"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9 9V5a1 1 0 112 0v4h4a1 1 0 110 2h-4v4a1 1 0 11-2 0v-4H5a1 1 0 110-2h4z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            New Task
+          </button>
+        </div>
+
+        <div class="w-1/2 ml-2 rounded-lg shadow-md p-4 mb-2 status">
+          <h1 class="text-xl font-bold mb-2">Done</h1>
+          <TaskCard
+            title="Design the mockup "
+            dueDate="2023-03-19"
+            completedAtDate="2023-03-18"
+            assigneeName="Rohit M"
+          />
+          <TaskCard
+            title="Get approval from principal"
+            dueDate="2023-03-19"
+            completedAtDate="2023-03-18"
+            assigneeName="Ajay S"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-react/src/TaskCard.js
@@ -0,0 +1,32 @@
+import './TaskCard.css'
+
+const TaskCard=(props)=>{
+    const months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var duedate = new Date(props.dueDate);
+    console.log(duedate)
+    const duedatenumber = duedate.getDate();
+    const duemonthnumber = duedate.getMonth();
+
+    var completeddate = new Date(props.completedAtDate);
+    const completedatenumber = completeddate.getDate();
+    const completemonthnumber = completeddate.getMonth();
+    if(duedatenumber >= completedatenumber && duemonthnumber >=completemonthnumber)
+    {
+        var status = "Completed on: "+ completedatenumber+"th"+months[completemonthnumber];
+
+    }
+    else
+    {
+         status= "Due on: "+duedatenumber+"th"+months[duemonthnumber];;
+    }
+
+    return (
+        <div class="bg-white rounded-lg shadow-md p-4 mb-2 TaskItem">
+        <h2 class="font-bold mb-2">{props.title}</h2>
+        <p class="text-gray-600 mb-2">{status}</p>
+        <p class="text-gray-600 mb-2"><span class="font-bold">Assignee: </span>{props.assigneeName}</p>
+        </div>
+
+    )
+};
+ export default TaskCard
