f2a161f5408f18f9fc84a124cbdf6780740a7e80
saimall <rameshwaramsaimallik@gmail.com>
2023-04-09 13:30:45 +0530

    Added Smarter-tasks using class-based components

---
+++ smarter-tasks/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Task from "./Task";
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Task/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ smarter-tasks/src/Task.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Task extends React.Component{
+    render(){
+        return(
+            <div>Buy groceries</div>
+        )
+    }
+        
+}
+
+export default Task;
