1ae33e3e2f86ce7b853008feabc890039283a027
saimall <rameshwaramsaimallik@gmail.com>
2023-05-08 21:17:30 +0530

    Checking the requiremnt of VTA

--- smarter-tasks/src/TaskDetailsPage.tsx
+++ smarter-tasks/src/TaskDetailsPage.tsx
@@ -20,27 +20,34 @@ const TaskDetailsPage = () => {
   const task = taskAppState.tasks.find((task) => task.id === Number(id)); //converting string id which we got from url to number for conversion
   /* as task and description and date may be undefined so we use task?.title , task?.date ,task?.description to specify that*/
   return (
-    <div>
-      
-      <div>
-        <div className="bg-black rounded-lg shadow-md p-4 mb-2 TaskItem hover:bg-gray-200">
-            <h3 className="font-gray-800 mb-2 ">
-              {" "}
-              Title :  {task?.title}
-            </h3>
-            <p className="text-gray-800 mb-2 ">
-              {" "}
-              Description : 
-              {task?.description}
-            </p>
-            <p className="text-gray-800 mb-2 ">
-              Due On: 
-              {task?.dueDate}
-            </p>
-         
-        </div>
+    <div className="bg-gray shadow-md rounded-md p-4">
+      <div className="flex justify-between items-center mb-4">
+        <h3 className="text-lg font-mediumm" >{task?.title}</h3>
       </div>
+      <p className="text-gray-600">{task?.description}</p>
+      <p className="text-gray-600">{task?.dueDate}</p>
     </div>
+    // <div>
+      
+    //   <div>
+    //     <div className="bg-black rounded-lg shadow-md p-4 mb-2 TaskItem hover:bg-gray-200">
+    //         <h3 className="font-gray-800 mb-2 ">
+    //           {" "}
+    //           Title :  {task?.title}
+    //         </h3>
+    //         <p className="text-gray-800 mb-2 ">
+    //           {" "}
+    //           Description : 
+    //           {task?.description}
+    //         </p>
+    //         <p className="text-gray-800 mb-2 ">
+    //           Due On: 
+    //           {task?.dueDate}
+    //         </p>
+         
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
