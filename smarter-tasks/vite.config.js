6280f9b7b891ff2d6b14b8113a9bfbec27ae3e1f
Saimall <rameshwaramsaimallik@gmail.com>
2023-07-01 14:45:38 +0530

    Finding the bugs for deployment

--- smarter-tasks/vite.config.js
+++ smarter-tasks/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
   return defineConfig({
-    build: {
-        outDir: 'build',
-      },
     plugins: [react()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
